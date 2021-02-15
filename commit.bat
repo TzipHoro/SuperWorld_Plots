db55018edf4c8046fa26ba287f5f301db6f09b27
Rebecca Horowitz <tziphoro@gmail.com>
2021-02-15 13:04:28 -0500

    `date +"m-d"

--- commit.bat
+++ commit.bat
@@ -1,13 +1,7 @@
 cd SuperWorld_Plots
 
-function Get-TimeStamp {
-    
-    return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
-    
-}
-
 git add .
-git commit -am "Regular auto-commit $(Get-TimeStamp)"
+git commit -m "`date +\"%Y-%m-%d\"
 git push
 
 pause
