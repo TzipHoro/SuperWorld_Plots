de7fb789ea0c615a67a526b495a831a8e80701ce
Rebecca Horowitz <tziphoro@gmail.com>
2021-02-15 13:39:00 -0500

    plot auto-commit

--- commit.bat
+++ commit.bat
@@ -1,7 +1,9 @@
-cd SuperWorld_Plots
+Rscript -e "rmarkdown::render('C:\\Users\\rebec\\SuperWorld_Plots\\index.Rmd')"
+
+cd C:\\Users\\rebec\\SuperWorld_Plots
 
 git add .
-git commit -m "Plot auto-commit"
+git commit -m "plot auto-commit"
 git push
 
 pause
