a907fb8f46c7b9854e05aee7d48c9e3727bc96aa
tziphoro <tziphoro@gmail.com>
2021-02-23 17:53:02 -0500

    plot auto-commit

--- commit.bat
+++ commit.bat
@@ -1,3 +1,5 @@
+"C:\Users\rebec\anaconda3\python.exe" "C:\Users\rebec\SuperWorld_Plot_Recommendation\data\SuperWorld_Plots_Sold.py"
+
 Rscript -e "rmarkdown::render('C:\\Users\\rebec\\SuperWorld_Plots\\index.Rmd')"
 
 cd C:\\Users\\rebec\\SuperWorld_Plots
